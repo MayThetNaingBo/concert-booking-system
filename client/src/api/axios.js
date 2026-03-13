571337d549c4b16310187d27c7269ae65b5cec43
MayThetNaingBo <maythetnaingbo04@gmail.com>
2026-03-14 04:00:54 +0800

    Fix Axio Base Url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://concert-booking-api.onrender.com"
+  baseURL: "https://concert-booking-api.onrender.com/api"
 });
 
 export default api;
